341a4682ff8c8ca57376178e7dc4f397437b0772
Marcin Rudnicki <marcin.rudnicki@amsterdam-standard.pl>
2020-07-01 15:00:37 +0200

    first commit

---
+++ Carthage/Build/iOS/fram.framework/Headers/fram.h
@@ -0,0 +1,19 @@
+//
+//  fram.h
+//  fram
+//
+//  Created by Marcin Rudnicki on 01/07/2020.
+//  Copyright © 2020 Marcin Rudnicki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for fram.
+FOUNDATION_EXPORT double framVersionNumber;
+
+//! Project version string for fram.
+FOUNDATION_EXPORT const unsigned char framVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fram/PublicHeader.h>
+
+
---
+++ fram/Testxxx.swift
@@ -0,0 +1,17 @@
+//
+//  Testxxx.swift
+//  fram
+//
+//  Created by Marcin Rudnicki on 01/07/2020.
+//  Copyright © 2020 Marcin Rudnicki. All rights reserved.
+//
+
+import UIKit
+
+public class Testxxx: NSObject {
+
+    public func xxTestFunc() {
+        print("xxxxx")
+    }
+    
+}
